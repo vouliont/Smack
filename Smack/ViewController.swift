d4af56888245f08ba1d39bdc24c602a9514fada2
vlad <vladporaiko@gmail.com>
2019-01-16 11:12:09 +0200

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Владислав on 1/16/19.
+//  Copyright © 2019 vladporaiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
